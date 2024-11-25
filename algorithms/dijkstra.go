a1c6d0d41fe7da49af35ee97ba2f64c97b9d972d
pemiinem <190139323@aluno.unb.br>
2024-11-25 13:26:38 -0300

    refactor: reorganize maze functionality into a separate package and remove unused DFS implementation

--- algorithms/dijkstra.go
+++ algorithms/dijkstra.go
@@ -5,32 +5,34 @@ import (
 	"time"
 
 	"github.com/faiface/pixel"
-	"github.com/projeto-de-algoritmos-2024/Grafos2_GoMazing/main"
+	"github.com/projeto-de-algoritmos-2024/Grafos2_GoMazing/maze"
 	"golang.org/x/image/colornames"
 )
 
-func (l *main.Maze) dijkstra(start, end [2]int) {
+func Dijkstra(m *maze.Maze, start, end [2]int) {
 	pq := &PriorityQueue{}
 	heap.Init(pq)
 	heap.Push(pq, &PriorityQueueItem{priority: 0, point: start})
+
 	distances := map[[2]int]int{start: 0}
 	previous := map[[2]int][2]int{}
 	visited := make(map[[2]int]bool)
 
 	for pq.Len() > 0 {
-		if l.win.Closed() {
+		if m.Win.Closed() {
 			return
 		}
 
 		currentItem := heap.Pop(pq).(*PriorityQueueItem)
 		current := currentItem.point
+
 		if visited[current] {
 			continue
 		}
 		visited[current] = true
 
 		x, y := current[0], current[1]
-		l.drawCell(x, y, pixel.ToRGBA(colornames.Green)) // Convert color.RGBA to pixel.RGBA
+		m.DrawCell(x, y, pixel.ToRGBA(colornames.Green))
 		time.Sleep(10 * time.Millisecond)
 
 		if current == end {
@@ -41,15 +43,15 @@ func (l *main.Maze) dijkstra(start, end [2]int) {
 			}
 			path = append(path, start)
 			for i := len(path) - 1; i >= 0; i-- {
-				l.drawCell(path[i][0], path[i][1], pixel.ToRGBA(colornames.Blue)) // Convert color.RGBA to pixel.RGBA
+				m.DrawCell(path[i][0], path[i][1], pixel.ToRGBA(colornames.Blue))
 				time.Sleep(5 * time.Millisecond)
 			}
 			return
 		}
 
-		for _, d := range l.directions {
+		for _, d := range m.Directions {
 			nx, ny := x+d[0], y+d[1]
-			if l.isWithinBounds(nx, ny) && l.grid[ny][nx] == 1 {
+			if m.IsWithinBounds(nx, ny) && m.Grid[ny][nx] == 1 {
 				neighbor := [2]int{nx, ny}
 				newDistance := distances[current] + 1
 				if _, ok := distances[neighbor]; !ok || newDistance < distances[neighbor] {
